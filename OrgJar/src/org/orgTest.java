ab8f4a5fbf53e917ef85576571efea8fd3efe804
sanghegde <41577414+sanghegde@users.noreply.github.com>
2018-08-04 16:54:09 +0530

    Add files via upload

---
+++ OrgJar/src/org/orgTest.java
@@ -0,0 +1,13 @@
+package org;
+
+import org2.OrgJar;
+
+public class orgTest {
+
+	public static void main(String[] args) {
+		OrgJar oj = new OrgJar();
+		oj.start();
+
+	}
+
+}
---
+++ OrgJar/src/org2/OrgJar.java
@@ -0,0 +1,9 @@
+package org2;
+
+public class OrgJar {
+
+	public void start() {
+		System.out.println("Start");
+	}
+
+}
