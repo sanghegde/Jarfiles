c25ae27e12a5674cb3a2f87cc897f5ef8b191379
sanghegde <41577414+sanghegde@users.noreply.github.com>
2018-08-04 08:46:49 +0530

    Add files via upload

---
+++ EmpJar/src/com/capgemini/EmpJar.java
@@ -0,0 +1,9 @@
+
+//jar file
+package com.capgemini;
+
+public class EmpJar {
+	public void disp() {
+		System.out.println("employee");
+	}
+}
---
+++ EmpJar/src/com/capgemini1/empTest.java
@@ -0,0 +1,10 @@
+package com.capgemini1;
+
+import com.capgemini.EmpJar;
+
+public class empTest {
+	public static void main(String args[]) {
+		EmpJar ej = new EmpJar();
+		ej.disp();
+	}
+}
